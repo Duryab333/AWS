08d2e469ac723c03f4d12fe383f495e02aa87505
Duryab Tariq <85177962+Duryab333@users.noreply.github.com>
2025-05-31 00:36:54 +0200

    Update outputs.tf

--- Terraform/Projects/aws_ec2/outputs.tf
+++ Terraform/Projects/aws_ec2/outputs.tf
@@ -1,10 +1,16 @@
 output "ec2_public_ip" {
-    description = "Public IP of Ec2 instance"
-    value = aws_instance.terraform_instance.public_ip    
+  description = "Public IP of Ec2 instance"
+  value       = [ for instance in aws_instance.terraform_instance :  instance.public_ip ]
+  # value = aws_instance.terraform_instance.public_ip  ---- for just 1 instance 
 }
 
 output "ec2_public_dns" {
-    description = "Public DNS value of Ec2 instance"
-    value = aws_instance.terraform_instance.public_dns
-  
+  description = "Public DNS value of Ec2 instance"
+#  value      = aws_instance.terraform_instance.public_dns
+   value      = [ for instance in aws_instance.terraform_instance : instance.public_dns ]
+
+}
+output "ec2_private_ip" {
+    #value = aws_instance.terraform_instance.private_ip
+    value =  [ for instance in aws_aws_instance.terraform_instance : instance.private_ip ]
 }
